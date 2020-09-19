6ff8fabcfffca0f0e5fb3d8f8c06833ea2580c14
George <zxj@qq.com>
2020-09-19 15:39:46 +0800

    chore: code comment

--- src/platforms/web/entry-runtime-with-compiler.js
+++ src/platforms/web/entry-runtime-with-compiler.js
@@ -14,6 +14,7 @@ const idToTemplate = cached(id => {
   return el && el.innerHTML
 })
 
+// 重写$mount方法
 const mount = Vue.prototype.$mount
 Vue.prototype.$mount = function (
   el?: string | Element,
